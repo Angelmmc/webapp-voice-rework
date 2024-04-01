e73644c8822540e46109904f306be8b88f475bef
Angelmmc <ammcardenas@hotmail.com>
2024-04-01 15:16:58 -0600

    insert action into Json Mock api method added

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,46 +18,13 @@ document.addEventListener('DOMContentLoaded', function () {
 
                 console.log('Orden identificada:', result);
 
-                const fechaHoraActual = new Date();
-                const fechaHoraFormateada = fechaHoraActual.toLocaleString();
-
-                console.log(fechaHoraActual);
 
                 switch (true) {
                     //Cambia el tamaño del texto al 5 de bootstrap al decir "tamaño 5"
                     case result.includes("tamaño 5"):
                         orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
                         controlTexto.innerHTML = '<span class="fs-5 fw-bold fst-italic">Beto mi patrón</span>';
-
-                        const recurso = {
-                            id: 1,
-                            accion: "Cambiar tamaño de letra",
-                            fecha: fechaHoraFormateada
-                          };
-                          
-                          // Paso 2: Convertir a JSON
-                          const recursoJSON = JSON.stringify(recurso);
-                          
-                          // Paso 3: Enviar la solicitud HTTP
-                          fetch('https://660b0491ccda4cbc75dc4478.mockapi.io/accion', {
-                            method: 'POST',
-                            headers: {
-                              'Content-Type': 'application/json'
-                            },
-                            body: recursoJSON
-                          })
-                          .then(response => {
-                            if (!response.ok) {
-                              throw new Error('Error al subir el recurso');
-                            }
-                            return response.json();
-                          })
-                          .then(data => {
-                            console.log('Recurso subido exitosamente:', data);
-                          })
-                          .catch(error => {
-                            console.error('Error:', error);
-                          });
+                        insertarJson("Cambiar tamaño de texto");
 
                         break;
                     //Abre facebook al decir "Abre Facebook"
@@ -94,4 +61,41 @@ document.addEventListener('DOMContentLoaded', function () {
             alert('El reconocimiento de voz no es soportado por este navegador.');
         }
     });
+
+    function insertarJson(accion) {
+        const fechaHoraActual = new Date();
+        const fechaHoraFormateada = fechaHoraActual.toLocaleString();
+
+        const recurso = {
+            id: 1,
+            accion: accion,
+            fecha: fechaHoraFormateada
+        };
+
+        // Paso 2: Convertir a JSON
+        const recursoJSON = JSON.stringify(recurso);
+
+        // Paso 3: Enviar la solicitud HTTP
+        fetch('https://660b0491ccda4cbc75dc4478.mockapi.io/accion', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: recursoJSON
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Error al subir el recurso');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Recurso subido exitosamente:', data);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }
+
+
 });
