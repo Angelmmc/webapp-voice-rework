43238f8ab0596393036ca9ddaf6f3b6e55b67034
Angelmmc <ammcardenas@hotmail.com>
2024-04-01 13:13:25 -0600

    add saving action on mock API

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,11 +18,47 @@ document.addEventListener('DOMContentLoaded', function () {
 
                 console.log('Orden identificada:', result);
 
+                const fechaHoraActual = new Date();
+                const fechaHoraFormateada = fechaHoraActual.toLocaleString();
+
+                console.log(fechaHoraActual);
+
                 switch (true) {
                     //Cambia el tamaño del texto al 5 de bootstrap al decir "tamaño 5"
                     case result.includes("tamaño 5"):
                         orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
                         controlTexto.innerHTML = '<span class="fs-5 fw-bold fst-italic">Beto mi patrón</span>';
+
+                        const recurso = {
+                            id: 1,
+                            accion: "Cambiar tamaño de letra",
+                            fecha: fechaHoraFormateada
+                          };
+                          
+                          // Paso 2: Convertir a JSON
+                          const recursoJSON = JSON.stringify(recurso);
+                          
+                          // Paso 3: Enviar la solicitud HTTP
+                          fetch('https://660b0491ccda4cbc75dc4478.mockapi.io/accion', {
+                            method: 'POST',
+                            headers: {
+                              'Content-Type': 'application/json'
+                            },
+                            body: recursoJSON
+                          })
+                          .then(response => {
+                            if (!response.ok) {
+                              throw new Error('Error al subir el recurso');
+                            }
+                            return response.json();
+                          })
+                          .then(data => {
+                            console.log('Recurso subido exitosamente:', data);
+                          })
+                          .catch(error => {
+                            console.error('Error:', error);
+                          });
+
                         break;
                     //Abre facebook al decir "Abre Facebook"
                     case result.includes("Abre Facebook"):
@@ -36,6 +72,14 @@ document.addEventListener('DOMContentLoaded', function () {
                     case result.includes("Cerrar pestaña actual"):
                         orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
                         window.close();
+                        break;
+                    case result.includes("cerrar navegador"):
+                        orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
+                        window.open('', '_self').close();
+                        break;
+                    case result.includes("cerrar navegador"):
+                        orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
+                        window.open('', '_self').close();
                         break;
                     default:
                         // Código a ejecutar si result no incluye ninguna de las keywords
